e7407b332ca3ad4b42c060695a967290e2b7a494
ren7087 <wakamakiren@icloud.com>
2024-02-13 18:48:25 +0900

    文字のアニメーション

--- src/components/organisms/Welcome.tsx
+++ src/components/organisms/Welcome.tsx
@@ -1,35 +1,60 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import GlovalImage from "../../assets/global.mp4";
+import GlobalImage from "../../assets/global.mp4";
 
 const Welcome = () => {
-  const maskRef = useRef(null); // マスキングテープ要素への参照
-  const containerRef = useRef(null); // welcome-container要素への参照
+  const containerRef = useRef(null);
+  const linesRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const textsRefs = useRef<(HTMLSpanElement | null)[]>([]);
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-    // アニメーションのタイムラインを作成し、ScrollTriggerと連携
-    gsap
-      .timeline({
-        scrollTrigger: {
-          trigger: containerRef.current, // トリガーとなる要素を設定
-          start: "top bottom", // ビューポートのtopがトリガー要素のcenterに達した時に開始
-          end: "bottom top", // トリガー要素のbottomがビューポートのtopに達した時に終了
-          toggleActions: "play none none reset",
-        },
-        defaults: { ease: "power2.inOut" },
-      })
-      .to(maskRef.current, {
-        width: "100%",
-        duration: 0.5,
-      })
-      .to(maskRef.current, {
-        scaleX: 0,
-        duration: 0.3,
-        transformOrigin: "right",
-      });
+    textsRefs.current.forEach((text, index) => {
+      if (text && linesRefs.current[index]) {
+        const tl = gsap.timeline({
+          scrollTrigger: {
+            trigger: text,
+            start: "top bottom",
+            end: "bottom top",
+            toggleActions: "play none none reset",
+          },
+          defaults: { ease: "power2.inOut" },
+        });
+
+        // マスキングテープのアニメーションを設定
+        tl.fromTo(
+          linesRefs.current[index],
+          { scaleX: 0, transformOrigin: "left" },
+          {
+            scaleX: 1,
+            duration: 0.5,
+            ease: "none",
+          }
+        )
+          .fromTo(
+            text,
+            { opacity: 0 },
+            {
+              opacity: 1,
+              duration: 0.5,
+            },
+            "<" // マスキングテープのアニメーションと同時に開始
+          )
+          .to(linesRefs.current[index], {
+            scaleX: 0,
+            duration: 0.3,
+            transformOrigin: "right",
+            ease: "none",
+          })
+          .to(
+            text,
+            { opacity: 1, duration: 0.3 },
+            ">" // マスキングテープが閉じるときにテキストを非表示にする
+          );
+      }
+    });
 
     return () => ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
   }, []);
@@ -48,26 +73,29 @@ const Welcome = () => {
         className="absolute inset-0 w-full h-full object-cover z-0"
         controls={false}
       >
-        <source src={GlovalImage} type="video/mp4" />
+        <source src={GlobalImage} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="relative z-10 flex flex-col justify-center h-full">
         <div className="px-6 md:px-20 py-10 md:py-40 font-bold text-xl md:text-6xl text-white text-right overflow-hidden">
-          <div className="relative inline-block">
-            <span className="z-20">ようこそ</span>
-            <br />
-            <span className="z-20">
-              デジタル化がもたらす
-              <br />
-              ビジネス変革の時代へ
-            </span>
-            {/* マスキングテープ */}
-            <div
-              ref={maskRef}
-              className="absolute top-0 left-0 h-full bg-primarySkyBlue"
-              style={{ zIndex: 10 }}
-            ></div>
-          </div>
+          {/* 各行を個別のdivで囲み、それぞれに ref を割り当てる */}
+          {["ようこそ", "デジタル化がもたらす", "ビジネス変革の時代へ"].map(
+            (line, index) => (
+              <div key={index} className="relative overflow-hidden">
+                <span
+                  ref={(el) => (textsRefs.current[index] = el)}
+                  className="z-20 block"
+                >
+                  {line}
+                </span>
+                <div
+                  ref={(el) => (linesRefs.current[index] = el)}
+                  className="absolute top-0 h-full bg-primarySkyBlue"
+                  style={{ zIndex: 10, width: "100%", left: "50%" }}
+                ></div>
+              </div>
+            )
+          )}
         </div>
       </div>
     </div>
